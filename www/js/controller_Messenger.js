4ea879a90358a9584a8fbf87bc49a81fe08eda1d
X-Y <x.yang@arduino.cc>
2016-02-23 13:23:30 +0100

    Fixed messenger to be read by Arduino

--- www/js/controller_Messenger.js
+++ www/js/controller_Messenger.js
@@ -20,11 +20,12 @@ angular.module('bleTest.controllers')
   	BleServices.writeData($scope.peripheral.id, data, MessengerService, $scope.readVal, onError);
     
     function str2ab(str) {
-      var buf = new ArrayBuffer(str.length); // 2 bytes for each char
+      var buf = new ArrayBuffer(str.length+1); // 2 bytes for each char
       var bufView = new Uint8Array(buf);
       for (var i=0, strLen=str.length; i<strLen; i++) {
         bufView[i] = str.charCodeAt(i);
       }
+      bufView[i]=0;
       return buf;
     }
   }
